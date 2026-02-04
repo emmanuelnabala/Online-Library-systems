534303a656929ed6fe701cca79009a2db96d8940
emmanuelnabala <emmanuelnabala@zetech.ac.ke>
2026-02-04 19:46:53 +0300

    Add Library class with display method

---
+++ Library.h
@@ -0,0 +1,16 @@
+#ifndef LIBRARY_H
+#define LIBRARY_H
+
+#include "Book.h"
+#include "User.h"
+#include <iostream>
+using namespace std;
+
+class Library {
+public:
+    void display() {
+        cout << "Library system running successfully." << endl;
+    }
+};
+
+#endif
